0adb39dacb0197b556c62a5c97012d0a03226f88
Felipe Bueno <felipebueno2201@gmail.com>
2022-09-16 00:04:12 -0300

    Components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+import Corpo from "./Corpo";
+import Mobile from "./Mobile";
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Corpo />
+      <Mobile />
+    </div>
+  );
+}
